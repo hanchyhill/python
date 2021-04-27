fec68adc2a0885e439458b9d3273c6956db43a87
minhill <lrrq369@gmail.com>
2021-04-27 16:06:04 +0800

    [add] 转换为气候标准格式

--- seafog/src/conver2CF.py
+++ seafog/src/conver2CF.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 
 config = {
     'sstk':{
@@ -47,12 +48,12 @@ config = {
         '_FillValue':  -999.9,
         'valid_min': 0.0,
         'standard_name': 'relative_humidity',
-        'units': '1',
+        'units': '0.01',
         'long_name': 'relative_humidity',
         'short_name': 'RH',
     },
     'temp':{
-        'name':'rhum',
+        'name':'temp',
         'missing_value': -999.9,
         '_FillValue':  -999.9,
         'valid_min': 0.0,
@@ -65,7 +66,47 @@ config = {
 
 file0 = '201810-202101_lon110.25_lat20.125.sstk.nc'
 file1 = '202102-202103_lon110.25_lat20.125.sstk.nc'
+dirPath = 'H:/github/python/seafog/data/'
 
 # ds.fillna(value=values)
 # da.where(x < 0, np.nan)
 # da.fillna(-999.9)
+
+def createVarNameList(varNamePrefix = 'sstk'):
+    timeList = list(range(0, 72+1, 3)) + list(range(78, 240+1, 6))
+    varNameList = list(map(lambda time: '{}{:0>3d}'.format(varNamePrefix, time), timeList))
+    return varNameList
+
+def convertDataArray(dataArray, daConfig, dropLevel: True):
+    '''
+    转换格式
+    '''
+    if(dropLevel):
+        dataArray = dataArray[:,0]
+    dataArray = xr.where(dataArray>daConfig['valid_min'], dataArray,np.nan)
+    dataArray.fillna(daConfig['missing_value'])
+    dataArray.attrs = daConfig
+    return dataArray
+    pass
+
+def openDataSet(dirPath = 'H:/github/python/seafog/data/', file = '201810-202101_lon110.25_lat20.125.sstk.nc',varNameSuffix='sstk',dropLevel=True):
+    ds = xr.open_dataset(dirPath + file)
+    iConfig = config[varNameSuffix]
+    nameList = createVarNameList(iConfig['name'])
+    for varName in nameList:
+        ds[varName] = convertDataArray(ds[varName], iConfig, dropLevel)
+    return ds
+
+def concatDataSet():
+    name = 't2md'
+    dataset01 = openDataSet(dirPath, '201810-202101_lon110.25_lat20.125.t2md.nc', name)
+    dataset02 = openDataSet(dirPath, '202102-202103_lon110.25_lat20.125.t2md.nc', name)
+    newDataset = xr.concat([dataset01, dataset02], dim="time")
+    newDataset.to_netcdf('201810-202103_lon110.25_lat20.125.{}.nc'.format(name))
+
+def openPressureDataSet():
+    name = 'rhum'
+    dataset = openDataSet(dirPath, '201810-202103_lon110.25_lat20.25.rhum.nc', name, False)
+    dataset.to_netcdf('201810-202103_lon110.25_lat20.25.{}.nc'.format(name))
+
+openPressureDataSet()
