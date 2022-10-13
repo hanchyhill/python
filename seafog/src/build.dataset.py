4519e75a4c927ffbd7efa429900647ca28b83786
minhill <lrrq369@gmail.com>
2022-10-13 17:48:05 +0800

    update: 生成dataset 训练集验证集测试集

--- seafog/src/build.dataset.py
+++ seafog/src/build.dataset.py
@@ -9,3 +9,6 @@ import pandas as pd
 # 合并所有数据
 # 分类雾，轻雾，无雾天气
 # 训练集train, 测试集valid, 验证集test分组
+# DataFrame.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
+# fcHour_list = list(range(0, 72+1, 3)) + list(range(78, 240+1, 6))
+# fcHour_list = list(range(0, 72+1, 3)) + list(range(78, 168+1, 6))
