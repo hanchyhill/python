d19ab2173c6a66e66f081a45545f7ee92f27051d
minhill <lrrq369@gmail.com>
2022-10-29 20:18:28 +0800

    update: 2015-2022数据集训练

--- seafog/src/merge.netcdf_fulltime.py
+++ seafog/src/merge.netcdf_fulltime.py
@@ -19,6 +19,15 @@ from metpy.units import units
 # 转换成pandas数据
 # 根据时间检索出实况的能见度数据作为检验合并入其中df_G7425.loc[df_G7425.index[0]]
 # 导出 csv 数据，每个起报时间csv文件
+if os.environ['COMPUTERNAME'] == 'DESKTOP-EQAO3M5':
+  computer_flag = 'home'
+else:
+  computer_flag = 'office'
+
+if computer_flag == 'home':
+  file_dir = "F:/github/pythonScript/seafog/"
+else:
+  file_dir = "H:/github/python/seafog/"
 
 fcHour_list = np.array(range(0, 240+1, 1))# list(range(0, 72+1, 3)) + list(range(78, 240+1, 6))
 # fcHour_list = list(range(0, 72+1, 3)) + list(range(78, 168+1, 6))
@@ -31,40 +40,48 @@ for multi_name_key in multi_level_name_list:
     for iLevel in levels_selected:
         multi_level_columns.append(f'{multi_name_key}{int(iLevel)}')
 
-dir_path = 'H:/github/python/seafog/data/CFdata/concat/201508-202205/'
+dir_path = os.path.normpath(os.path.join(file_dir, './data/CFdata/concat/201508-202205/'))
 
-file_59754 = 'H:/github/python/seafog/data/station/59754.20130101-2022093023.csv'
+file_59754 = os.path.normpath(os.path.join(file_dir, './data/station/59754.20130101-2022093023.csv'))
 df_59754 = pd.read_csv(file_59754,sep=',',na_values=[9999])
 df_59754.index = pd.to_datetime(df_59754["DDATETIME"])
 # print(df_59754.dtypes)
 
-h5_store = pd.HDFStore('H:/github/python/seafog/data/collection/fullhour/fullhour.ec201508-202205.hdf', mode='w')
+h5_store = pd.HDFStore(os.path.normpath(os.path.join(file_dir, './data/collection/fullhour/full.suf110.25_20.125_pre110.25_20.25_59754_201508-202205.hdf')), mode='w')
 
 def get_time_list(ds):
     da_time = ds['time']
     return da_time
+
+def get_align_time(ds_list):
+    align_time = ds_list[0]['time']
+    for i_ds in ds_list:
+        i_time = i_ds['time']
+        align_time, _ = xr.align(align_time, i_time)
+    return align_time
 
 def get_elems():
     pass
 
 def convert_nc2csv():
     file_name = 'fullhour.201508-202205_lon110.25_lat20.125.sstk.nc'
-    ds_time = xr.open_dataset(f'{dir_path}{file_name}')
-    init_time = pd.Series(ds_time['time'])
+    ds_time = xr.open_dataset(os.path.join(dir_path, file_name))
+    # init_time = pd.Series(ds_time['time'])
     ds_single_list = []
     
     for name in single_level_name_list:
         file_name = f'fullhour.201508-202205_lon110.25_lat20.125.{name}.nc'
-        ids = xr.open_dataset(f'{dir_path}{file_name}')
+        ids = xr.open_dataset(os.path.join(dir_path, file_name))
         ds_single_list.append(ids)
 
     ds_multi_list = []
     
     for name in multi_level_name_list:
         file_name = f'fullhour.201508-202205_lon110.25_lat20.25.{name}.nc'
-        ids = xr.open_dataset(f'{dir_path}{file_name}')
+        ids = xr.open_dataset(os.path.join(dir_path, file_name))
         ds_multi_list.append(ids)
-
+    ds_all = ds_single_list + ds_multi_list
+    init_time = get_align_time(ds_all)
     ds_t = ds_multi_list[multi_level_name_list.index('temp')]
     ds_rh = ds_multi_list[multi_level_name_list.index('rhum')]
 
@@ -75,14 +92,14 @@ def convert_nc2csv():
             name_key = single_level_name_list[index]
             i_ds = ds_single_list[index]
             i_varname = f'{name_key}{iHour:0>3d}'
-            da = i_ds[i_varname]
+            da = i_ds[i_varname].sel(time = init_time)
             series_map[name_key] = da.values
         # TODO: temp. and rhum in 850hPa and 925hPa 计算逆温情况
         for index in range(len(multi_level_name_list)):
             name_key = multi_level_name_list[index]
             i_ds = ds_multi_list[index]
             i_varname = f'{name_key}{iHour:0>3d}'
-            da = i_ds[i_varname]
+            da = i_ds[i_varname].sel(time = init_time)
             for iLevel in levels_selected:
                 iVarname = f'{name_key}{int(iLevel)}'
                 series_map[iVarname] = da.sel(level = iLevel).values
@@ -91,8 +108,8 @@ def convert_nc2csv():
             # series_map[name_key] = da.values
         # TODO: 计算相当位温
         
-        da_t  = ds_t[f'temp{iHour:0>3d}']
-        da_rh = ds_rh[f'rhum{iHour:0>3d}']
+        da_t  = ds_t[f'temp{iHour:0>3d}'].sel(time = init_time)
+        da_rh = ds_rh[f'rhum{iHour:0>3d}'].sel(time = init_time)
         for iLevel in levels_selected:
             iVarname = f'theta_e{int(iLevel)}'
             rhum = np.clip(da_rh.sel(level = iLevel), 0, 100)*units.percent
@@ -135,7 +152,7 @@ def convert_nc2csv():
         df.loc[df['station_vis']>10000,'station_vis_cat'] = 2
         df_rain_remove = df[df['station_rain1']<1]
         df_rain_remove.to_hdf(h5_store, key=f'df_{iHour:0>3d}', mode='a')
-        df_rain_remove.to_csv(f'ec201508-202205_xuwen_{iHour:0>3d}.csv')
+        df_rain_remove.to_csv(f'{dir_path}ec201508-202205_xuwen_{iHour:0>3d}.csv')
     h5_store.close()
 
 def get_station_data_by_time(time_list, key):
